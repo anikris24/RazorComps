058d8be34a68d3416f14f1b5e2da32f4ecb9fb8e
Anil Kumar  Konasale Krishna  (EXT) <anilkumar.konasalekrishna@aveva.com>
2025-05-12 10:42:18 -0700

    logger

--- calApp/Program.cs
+++ calApp/Program.cs
@@ -7,5 +7,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddLogging(logging =>
+            {
+                logging.SetMinimumLevel(LogLevel.Debug); // Set the minimum level for browser console
 
+                // You could potentially add custom providers here
+                // logging.AddProvider(new MyCustomBrowserLoggerProvider());
+            });
 await builder.Build().RunAsync();
